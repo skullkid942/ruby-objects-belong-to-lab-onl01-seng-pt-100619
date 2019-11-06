64d09f492faa939f25ea7d5aaabc3e102fddd7c8
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-06 16:46:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,10 @@ class Song
 
   attr_accessor :title, :artist
   
-  
+  def initialize(name, artist=nil)
+    @name = name
+    @artist = artist
+  end
   
   def artist
     puts title.artist.name 
